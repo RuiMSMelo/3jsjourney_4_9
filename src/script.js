f6a9bb5062985a8d4a8189b2d3e44d3c69764160
RuiMSMelo <ruimsmelo@gmail.com>
2025-05-05 19:34:06 +0100

    added 2 point light

--- src/script.js
+++ src/script.js
@@ -131,6 +131,24 @@ directionalLightHelper.material.side = THREE.DoubleSide
 directionalLightHelper.position.set(0, 0, 3)
 scene.add(directionalLightHelper)
 
+const pointLightHelper = new THREE.Mesh(
+  new THREE.IcosahedronGeometry(0.1, 2),
+  new THREE.MeshBasicMaterial()
+)
+pointLightHelper.material.color.setRGB(1.0, 0.1, 0.1)
+pointLightHelper.material.side = THREE.DoubleSide
+pointLightHelper.position.set(0, 2.5, 0)
+scene.add(pointLightHelper)
+
+const pointLightHelper2 = new THREE.Mesh(
+  new THREE.IcosahedronGeometry(0.1, 2),
+  new THREE.MeshBasicMaterial()
+)
+pointLightHelper2.material.color.setRGB(0.1, 1.0, 0.5)
+pointLightHelper2.material.side = THREE.DoubleSide
+pointLightHelper2.position.set(2, 2, 2)
+scene.add(pointLightHelper2)
+
 /**
  * Animate
  */
--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -5,6 +5,7 @@ varying vec3 vPosition;
 
 #include ../includes/ambientLight.glsl
 #include ../includes/directionalLight.glsl
+#include ../includes/pointLight.glsl
 
 void main()
 {
@@ -16,6 +17,8 @@ void main()
     vec3 light = vec3(0.0);
     light += ambientLight(vec3(1.0), 0.03);
     light += directionalLight(vec3(0.1, 0.1, 1.0), 1.0, normal, vec3(0.0, 0.0, 3.0), viewDirection, 20.0);
+    light += pointLight(vec3(1.0, 0.1, 0.1), 1.0, normal, vec3(0.0, 2.5, 0.0), viewDirection, 20.0, vPosition, 0.25);
+    light += pointLight(vec3(0.1, 1.0, 0.5), 1.0, normal, vec3(2.0, 2.0, 2.0), viewDirection, 20.0, vPosition, 0.2);
     color *= light;
 
     // Final color
